a99307e152e0977e33fdcfc8c1b5e9a0716a7a6c
su790 <jungthapasushant@gmail.com>
2023-03-31 23:46:24 +0545

    Completed upto pagination

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -15,7 +15,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts',unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -23,6 +23,12 @@ class Post(models.Model):
 
     objects=models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,
+                                                self.publish.month,
+                                                self.publish.day,
+                                                self.slug])
 
     class Meta:
         ordering = ['-publish']
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage
 
-
-def post_detail(request, id):
+def post_detail(request, year,month,day,post):
     try:
-        post = Post.published.get(id=id)
+        post = Post.published.get(slug=post,
+                                  publish__year=year,
+                                  publish__month=month,
+                                  publish__day=day)
     except:
         raise Http404("No post found")
 
@@ -13,5 +16,13 @@ def post_detail(request, id):
 
 
 def post_list(request):
-    post = Post.published.all()
+    posts = Post.published.all()
+    paginator = Paginator(posts,3)
+    page_number = request.GET.get('page')
+    post = paginator.get_page(page_number)
+
+        # try:
+        #     post = paginator.get_page(page_number)
+        # except EmptyPage:
+        #     post = paginator.get_page(paginator.num_pages)
     return render(request, "blog/post/list.html", {'post': post})
