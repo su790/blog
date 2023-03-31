fe6d6c3451cb951f9a93ebc48bf5bf6d17990092
su790 <jungthapasushant@gmail.com>
2023-03-31 21:15:07 +0545

    This commit is after making views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except:
+        raise Http404("No post found")
+
+    return render(request, "blog/post/detail.html", {'post': post})
+
+
+def post_list(request):
+    post = Post.published.all()
+    return render(request, "blog/post/list.html", {'post': post})
